2acb7eaa407bf8119cf8c47ca6f43b3d8cb3ea94
Santiagozm16 <55797102+Santiagozm16@users.noreply.github.com>
2021-05-10 16:05:57 -0500

    Proyecto Sistema Solar

---
+++ Sistema Solar/Assets/Scripts/Planeta.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planeta : MonoBehaviour
+{
+    [SerializeField] public Vector3 Posicion_Inicial; //Vector para inicializar desde unity
+    public float masa; //Variable para la masa del planeta
+    [SerializeField] public Vector3 Velocidad_Inicial; //Vector para inicializar desde unity
+
+    public Vector3 Posicion; //Vector de Posición
+    public Vector3 Velocidad; //Vector de Velocidad
+    private Vector3 fuerzaNeta; //Fuerza Neta
+
+    public Transform target; //Objeto sobre el cual rotar
+    public int Rotacion; //Velocidad de la rotación
+    void Update()
+    {
+
+        transform.RotateAround(target.transform.position, target.transform.up, Rotacion * Time.deltaTime); //Totación por cada frame
+    }
+
+    public Vector3 fuerzaG(Vector3 p1, Vector3 p2, float G, float m1, float m2)//Calculo de Fuerza gravitacional.
+    {
+        Vector3 r_vec = p1 - p2;
+        float r_mag = r_vec.magnitude;
+        Vector3 r_uni = r_vec / r_mag;
+        float F_Esc = G * m1 * m2 / Mathf.Pow(r_mag, 2);
+        Vector3 F = -F_Esc * r_uni;
+        fuerzaNeta = F;
+        return F;
+    }
+
+    //Get y Set del Planeta
+    public void cargarPosicion()
+    {
+        transform.position = Posicion;
+    }
+
+    void Start()
+    {
+        Posicion = Posicion_Inicial;
+        Velocidad = Velocidad_Inicial;
+    }
+
+    public Vector3 get_Posicion()
+    {
+        return Posicion;
+    }
+
+    public Vector3 get_Velocidad()
+    {
+        return Velocidad;
+    }
+    
+    public void set_Posicion(Vector3 aux)
+    {
+        Posicion = aux;
+    }
+    public void set_Velocidad(Vector3 aux)
+    {
+        Velocidad = aux;
+    }
+    public float get_Masa()
+    {
+        return masa;
+    }
+}
---
+++ Sistema Solar/Assets/Scripts/SistemaSolar.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SistemaSolar : MonoBehaviour
+{
+    public GameObject[] planetas;
+
+    [SerializeField] float paso = 0f;
+    [SerializeField] float G = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        planetas = GameObject.FindGameObjectsWithTag("Player");
+    }
+    //Variables Auxiliares para los calculos necesarios
+    Vector3 VelPlaneta1;
+    Vector3 VelPlaneta2;
+    float m1, m2;
+    Vector3 PosPlaneta1;
+    Vector3 PosPlaneta2;
+    Vector3 Fuerza1;
+    Vector3 Fuerza2;
+    Vector3 auxVel;
+    Vector3 auxPos;
+    // Update is called once per frame
+    void Update()
+    {
+            //planetas[0] = La tierra
+            //planetas[1] = El Sol
+            //Obtención de Variables:
+            PosPlaneta1 = planetas[0].GetComponent<Planeta>().get_Posicion();
+            PosPlaneta2 = planetas[1].GetComponent<Planeta>().get_Posicion();
+            VelPlaneta1 = planetas[0].GetComponent<Planeta>().get_Velocidad();
+            VelPlaneta2 = planetas[1].GetComponent<Planeta>().get_Velocidad();
+            m1 = planetas[0].GetComponent<Planeta>().get_Masa();
+            m2 = planetas[1].GetComponent<Planeta>().get_Masa();
+            
+            //Calculos de la fuerza Tierra  - Sol (Como el calculo es tierra sol, la Posición de la tierra es el parametro 1)
+            Fuerza1 = planetas[0].GetComponent<Planeta>().fuerzaG(PosPlaneta1, PosPlaneta2, G, m1, m2);
+
+            eulerSolver(paso, Fuerza1, PosPlaneta1, VelPlaneta1, m1);
+        
+            planetas[0].GetComponent<Planeta>().set_Posicion(auxPos);
+            planetas[0].GetComponent<Planeta>().set_Velocidad(auxVel);
+            planetas[0].GetComponent<Planeta>().cargarPosicion();
+
+            //Calculos de la fuerza Sol - Tierra (Como el calculo es sol tierra, la Posición del Sol es el parametro 1)
+            Fuerza2 = planetas[1].GetComponent<Planeta>().fuerzaG(PosPlaneta2, PosPlaneta1, G, m2, m1);
+
+            eulerSolver(paso, Fuerza2, PosPlaneta2, VelPlaneta2, m2);
+
+            planetas[1].GetComponent<Planeta>().set_Posicion(auxPos);
+            planetas[1].GetComponent<Planeta>().set_Velocidad(auxVel);
+            planetas[1].GetComponent<Planeta>().cargarPosicion();
+    }
+
+    public void eulerSolver(float step, Vector3 Fuerza, Vector3 p, Vector3 v, float mass) //Solucionador de Euler 
+    {
+        Vector3 aceleracion = Fuerza / mass;
+        Vector3 velPasada = v;
+        v += aceleracion * step;
+        p += velPasada * step;
+
+        auxVel = v;
+        auxPos = p;
+    }
+}
